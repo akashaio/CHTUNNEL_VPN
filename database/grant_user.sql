36369f76798d68922867dc11f4d1e9b33dc164f9
Chagridsada  Boonthus <mr.tumcpe@gmail.com>
2012-01-14 22:28:32 +0700

    Chtunnel VPN2.0 01-01-2012

--- database/grant_user.sql
+++ database/grant_user.sql
@@ -2,4 +2,5 @@
 -- database : chtunnel_vpn
 -- username : chtunnel_vpn
 -- password : chtunnel_vpn
-GRANT ALL ON chtunnel_vpn.* TO 'chtunnel_vpn'@'localhost' IDENTIFIED BY 'chtunnel_vpn'; 
+GRANT ALL ON chtunnel_vpn.* TO 'chtunnel_vpn'@'localhost' IDENTIFIED BY 'chtunnel_vpn';
+GRANT ALL ON chtunnel_vpn.* TO 'chtunnel_vpn'@'ns57.hostinglotus.net' IDENTIFIED BY 'chtunnel_vpn';
